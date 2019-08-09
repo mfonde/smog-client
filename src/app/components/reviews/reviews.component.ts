d8f6173af75d545bc7ca29221cee55505db4702d
Megan Fonde <meganfonde@gmail.com>
2019-08-09 11:04:08 -0400

    reviews for specific movies

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Review } from '../../models/review-model'
 import { ReviewService } from '../../services/review.service'
+
+import { MovieData } from '../../models/MovieData';
 
 @Component({
   selector: 'app-reviews',
@@ -10,16 +12,28 @@ import { ReviewService } from '../../services/review.service'
 export class ReviewsComponent implements OnInit {
 
   reviews: Review[];
+  @Input() displayedMovie: MovieData;
+  
 
   getAllReviews(): void {
     this.reviewService.getAllReviews()
       .subscribe(reviews => this.reviews = reviews)
   }
 
+  getReviewsByImdbID() {
+    console.log(this.displayedMovie.imdbID)
+    this.reviewService.getReviewsByImdbID(this.displayedMovie.imdbID)
+    .subscribe(reviews => this.reviews = reviews)
+  }
+
   constructor(private reviewService: ReviewService) { }
 
   ngOnInit() {
+    if(this.displayedMovie){
+      this.getReviewsByImdbID()
+    } else {
     this.getAllReviews()
+    }
   }
 
 }
