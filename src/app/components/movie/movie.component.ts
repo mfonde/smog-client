8267412a25e369af1c2dc66b04c62b7f17cf126d
Megan Fonde <meganfonde@gmail.com>
2019-08-12 10:20:53 -0400

    save to favorites working

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 import { DatabaseService } from 'src/app/services/database.service';
+import { FavoriteService } from '../../services/favorite.service';
+import { Favorite } from '../../models/favorite-model';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -9,17 +13,54 @@ import { DatabaseService } from 'src/app/services/database.service';
 })
 export class MovieComponent implements OnInit {
 
+  favoriteSelected = false;
+  favoriteAdded = false;
   public movie;
+  public selectRanking: FormGroup;
 
-  constructor(private databaseService: DatabaseService) { this.getMovie(); }
+  constructor(private databaseService: DatabaseService, private favoriteService: FavoriteService, private form: FormBuilder) {         this.getMovie();
+    this.createForm();
+  }
+
+  createForm() {
+    this.selectRanking = this.form.group({
+      ranking: new FormControl
+    })
+  }
 
   getMovie() {
     this.movie = this.databaseService.movie;
   }
 
+  onFavoriteSelected(){
+    this.favoriteSelected = true;
+  }
+
+  favoriteClose() {
+    this.favoriteSelected = false;
+  }
+
+addToFavorites() {
+ const movieTitle = this.databaseService.movie.title;
+ const imdbId = this.databaseService.movie.imdbID;
+ const poster = this.databaseService.movie.poster;
+ const ranking = this.selectRanking.value.ranking;
+
+  const newFavorite = new Favorite(movieTitle, poster, imdbId, ranking);
+  this.favoriteService.saveFavorite(newFavorite);
+  console.log(this.databaseService.movie.title);
+  console.log(ranking);
+  console.log(newFavorite);
+  this.favoriteClose();
+  this.favoriteAdded = true;
+
+}
+
   ngOnInit() {
     let x = this.databaseService.movie;
     console.log(x);
+    // console.log(this.databaseService.movie.title);
+
   }
 
 
