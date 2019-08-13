16a4a87c5416340829aa4279721db8fbbc7bc1c0
Suraj Marakani <skmarakani@gmail.com>
2019-08-13 13:19:46 -0400

    reviews added on profile page and profile services

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,29 +2,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const token = JSON.parse(localStorage.getItem('sessionToken'));
+const smallUser = JSON.parse(localStorage.getItem('currentUser'))
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-
+  
+  
   constructor(private http:HttpClient) { }
+  
 
+  public reviews: [];
+  public bigUser = smallUser.user.username
+  public smallUser = JSON.parse(localStorage.getItem('currentUser'))
+  
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
+    "Authorization": `${token}`
   })
-
-getUser(){
-const token = localStorage.getItem('currentUser.sessionToken')
-console.log(token)
-// const url = `http://localhost:3000/review/username/`
-//   return this.http.get<any>(url,
-//     {headers: this.headers})
-}
   
-// getYourReviews() {
-//   const url = `http://localhost:3000/review/username/${currentUser.username}`
-//   return this.http.get<any>(url,
-//     {headers:this.headers
-//     })
-//   }
+  getYourReview(middleUser){
+    const url = `http://localhost:3000/review/username/${middleUser}`
+    console.log(smallUser)
+    console.log(token)
+    console.log(middleUser, "work")
+      return this.http.get<any>(url,
+        {headers: this.headers})
+        // .subscribe(data =>{
+        //    this.reviews = data, console.log(data)
+        //})
+  }
+  
+  
+ 
   }
