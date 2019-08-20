4272307ee4eb2dbadfbaed08a24ed4fe020fe560
Megan Fonde <meganfonde@gmail.com>
2019-08-20 16:17:31 -0400

    fixed server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export let APIURL = '';
 
 switch (window.location.hostname) {
   case 'smog-moviethoughtclient.herokuapp.com':
-    APIURL = 'https://smog-moviethoughserver.herokuapp.com'
+    APIURL = 'https://smog-moviethoughtserver.herokuapp.com'
     break;
   default:
     APIURL = 'http://localhost:3000';
