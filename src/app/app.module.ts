50905df101188c8017c9e722cd8dd3daeddf9035
Suraj Marakani <skmarakani@gmail.com>
2019-08-20 14:25:12 -0400

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,6 +49,7 @@ import { FavoritesComponent } from './components/favorites/favorites.component';
 import { FavoriteItemComponent } from './components/favorites/favorite-item/favorite-item.component';
 import { NewReviewDialog } from '../app/components/movie/movie.component';
 import { FavoriteDialog } from '../app/components/movie/movie.component';
+import { UpdateDialog } from '../app/components/profile/profile.component';
 
 @NgModule({
   //TODO: DECLARES ALL OF THE COMPONENTS ADDED INTO THE APP MODULE 
@@ -70,7 +71,8 @@ import { FavoriteDialog } from '../app/components/movie/movie.component';
     ReviewItemComponent,
     FavoritesComponent,
     FavoriteItemComponent,
-    FavoriteDialog
+    FavoriteDialog,
+    UpdateDialog
   ],
 
   entryComponents:[
@@ -79,7 +81,8 @@ import { FavoriteDialog } from '../app/components/movie/movie.component';
     UserSearchComponent,
     HeaderComponent,
     AuthHeaderComponent,
-    FavoriteDialog
+    FavoriteDialog,
+    UpdateDialog
   ],
   //TODO: HOLDS ALL OF THE IMPORTS ADDED TO THE ANGULAR APP FOR OTHER FILES TO USE 
   imports: [
